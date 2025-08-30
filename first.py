5c654c8eeaff00b3b6f024a13c72bde283ffcad9
malikazizawan <pvtmalikazizawan@gmail.com>
2025-08-30 02:49:55 -0700

    Added Task 1

---
+++ first.py
@@ -0,0 +1,55 @@
+# Q No 01
+# Print Your Name with your Father name and Date of birth using suitable escape sequence charactor ?
+print("Name:\t\tAbdul Aziz Awan\nFather Name:\tAbdul Rasheed Awan\nDate of Birth:\t15-08-2002")
+
+
+# Q No 02
+# Write your small bio using variables and print it using print function?
+name = "Abdul Aziz Awan \n"
+f_name = "Abdul Rasheed Awan \n"
+date_of_barth = "07/07/2003 \n"
+want = "I Want To Be A Cloud Deta Eng \n"
+w_from = "I Am From Mianwali But live In Karachi \n"
+fav_sub = "My Fav Sub Computer"
+
+print(name, f_name, date_of_barth, want, w_from, fav_sub)
+
+# Q No 03
+# Write a program in which use all the operators we can use in Python ?
+a = 20
+b = 15
+
+print("Arithmetic Operators: \n")
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a % b =", a % b)
+print("a ** b =", a ** b)   
+print("a // b =", a // b)
+
+# Comparison Operators
+print("\nComparison Operators:")
+print("a == b:", a == b)
+print("a != b:", a != b)
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a >= b:", a >= b)
+print("a <= b:", a <= b)
+
+# Q No 04
+# Step 1: Mention Marks of English, Islamiat and Maths
+
+english = 85
+islamiat = 90
+maths = 95
+total_marks = 300
+obtained_marks = english + islamiat + maths
+percentage = (obtained_marks / total_marks) * 100
+
+print("Marks in English:", english)
+print("Marks in Islamiat:", islamiat)
+print("Marks in Maths:", maths)
+print("Total Marks:", total_marks)
+print("Obtained Marks:", obtained_marks)
+print("Percentage:", percentage, "%")
